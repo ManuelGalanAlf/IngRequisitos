ee8dd49d4d013a971fa2815d2cab4fabae414ba4
ManuelGalanAlf <manucorreoe@gmail.com>
2024-12-03 00:30:21 +0100

    Modificacion de Base de datos para SQL Server

--- Exportargrupo17DB/grupo17DB_Atributo.sql
+++ Exportargrupo17DB/grupo17DB_Atributo.sql
@@ -1,60 +1,15 @@
--- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
---
--- Host: database-minipim.cdwgeayaeh1v.eu-central-1.rds.amazonaws.com    Database: grupo17DB
--- ------------------------------------------------------
--- Server version	8.0.39
+-- Creación de la tabla Atributo con restricción CHECK para simular ENUM
+CREATE TABLE Atributo (
+  nombre varchar(255) NOT NULL,
+  tipo varchar(50) NOT NULL,
+  PRIMARY KEY (nombre),
+  CONSTRAINT CHK_Tipo CHECK (tipo IN ('Entero', 'Real', 'Cadena'))
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
-SET @@SESSION.SQL_LOG_BIN= 0;
-
---
--- GTID state at the beginning of the backup 
---
-
-SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '';
-
---
--- Table structure for table `Atributo`
---
-
-DROP TABLE IF EXISTS `Atributo`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Atributo` (
-  `nombre` varchar(255) NOT NULL,
-  `tipo` enum('Entero','Real','Cadena') NOT NULL,
-  PRIMARY KEY (`nombre`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Atributo`
---
-
-LOCK TABLES `Atributo` WRITE;
-/*!40000 ALTER TABLE `Atributo` DISABLE KEYS */;
-INSERT INTO `Atributo` VALUES ('Coste','Cadena'),('Detalle','Cadena'),('Precio','Real'),('Tamaño','Real');
-/*!40000 ALTER TABLE `Atributo` ENABLE KEYS */;
-UNLOCK TABLES;
-SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-12-03  0:05:21
+-- Insertando datos en la tabla Atributo
+INSERT INTO Atributo (nombre, tipo) 
+VALUES 
+  ('Coste', 'Cadena'),
+  ('Detalle', 'Cadena'),
+  ('Precio', 'Real'),
+  ('Tamaño', 'Real');
